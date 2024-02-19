2ae4a1c903b770a301c14f20ca43d6ed02a61c8b
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-02-19 13:57:13 -0800

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Amir Kabiri on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
